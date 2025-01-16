be70ec0da426e591975cc3faf48821c4f70bc963
Victor Ogunwehin <victorogunwehin@gmail.com>
2025-01-16 11:53:00 +0100

    updates

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,20 @@ import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { UsersModule } from '../users/users.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  imports: [PassportModule, JwtModule.register({ secret: 'jwtsecretkey' })],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: 'secretKey',
+      signOptions: { expiresIn: '1h' },
+    }),
+    PrismaModule,
+  ],
   controllers: [AuthController],
-  providers: [AuthService, PrismaService],
+  providers: [AuthService],
 })
 export class AuthModule {}
